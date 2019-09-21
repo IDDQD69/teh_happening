ea751fa72f09cca3fd12190a940a3a7a4b17564e
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-09-21 09:28:58 +0300

    Merge branch 'master' of C:\Users\Ville-Veikko\Desktop\HackAthon\teh_happening with conflicts.

--- frontend/front/src/router.js
+++ frontend/front/src/router.js
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 
-import { Redirect } from 'react-router-dom'
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import {Redirect} from 'react-router-dom'
 
-import { clear, getLogin } from 'storage'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+import {clear, getLogin} from 'storage'
 
 import isEmpty from 'lodash/isEmpty'
 
-import { storeLogin } from 'storage'
+
+import {storeLogin} from 'storage'
 
 import data from 'data.json'
 
@@ -17,94 +19,138 @@ import Event from 'containers/Event'
 import CreateEvent from 'containers/CreateEvent'
 import TopBar from 'components/TopBar'
 
-import { makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
-import Button from '@material-ui/core/Button'
-import TelegramLoginButton from 'react-telegram-login'
+import {makeStyles} from '@material-ui/core/styles'
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import TelegramLoginButton from "react-telegram-login";
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+
+
+
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {},
-  toolbar: theme.mixins.toolbar,
+    root: {
+        display: 'flex',
+    },
+    appBar: {},
+    toolbar: theme.mixins.toolbar,
+    login: {
+        position: 'absolute',
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -50%)'
+    },
+    signin: {
+        padding:'10%',
+        align: 'center',
+        justify: 'center',
+        margin: 'auto'
+    },
 }))
 
 function AppRouter(props) {
-  const {} = props
-  const classes = useStyles()
-  const [login, setLogin] = useState({})
+    const {} = props
+    const classes = useStyles()
+    const [login, setLogin] = useState({})
 
-  useEffect(() => {
-    setLogin(getLogin())
-  }, [])
+    useEffect(() => {
+        setLogin(getLogin())
+    }, [])
 
-  const doLogin = index => {
-    storeLogin(data[index])
-    setLogin(data[index])
-  }
 
-  const handleTelegramResponse = response => {
-    setLogin(response)
-    storeLogin(response)
-    props.history.push('/events/')
-  }
+    const doLogin = (index) => {
+        storeLogin(data[index])
+        setLogin(data[index])
+    }
 
-  const telegramLogin = (
-    <TelegramLoginButton
-      dataOnauth={handleTelegramResponse}
-      botName="TehHappeningBot"
-    />
-  )
+    const handleTelegramResponse = response => {
+        setLogin(response)
+        storeLogin(response)
+        props.history.push('/events/')
 
-  const loginButtons = props => {
-    return (
-      <Paper>
-        {data.map((user, i) => {
-          return (
-            <Button
-              key={i}
-              onClick={() => {
-                doLogin(i)
-                props.history.push('/events/')
-              }}
-            >
-              Login {user.username}
-            </Button>
-          )
-        })}
-      </Paper>
+    }
+
+    const telegramLogin = (
+        <TelegramLoginButton
+            dataOnauth={handleTelegramResponse}
+            botName='TehHappeningBot'/>
     )
-  }
 
-  const loginComponent = props => {
-    console.log('props', props)
-    return <div>{isEmpty(login) && loginButtons(props)}</div>
-  }
-
-  const handleLogout = () => {
-    clear()
-    setLogin({})
-  }
-
-  return (
-    <Router>
-      <div>
-        <TopBar login={login} handleLogout={handleLogout} />
-        <main className={classes.content}>
-          <Route path="/" component={loginComponent} />
-          {!isEmpty(login) && (
+    const loginButtons = (props) => {
+        return (
             <div>
-              <div className={classes.toolbar} />
-              <Route path="/event/new" exact component={CreateEvent} />
-              <Route path="/events" exact component={Events} />
-              <Route path="/events/:eventId" exact component={Event} />
+                <List component="nav" aria-label="main mailbox folders">
+                    {data.map((user, i) => {
+                        return <ListItem button key={i} onClick={() => {
+                            doLogin(i)
+                            props.history.push("/events/")
+                            }}>
+                            <Avatar edge="start"
+                                    alt="Remy Sharp"
+                                    src={user.photo_url}
+                                    className={classes.bigAvatar}
+                            />
+                            Login {user.username}
+
+                        </ListItem>
+                    })}
+                </List>
             </div>
-          )}
-        </main>
-      </div>
-    </Router>
-  )
+        )
+    }
+
+
+    const loginComponent = (props) => {
+        console.log('props', props)
+        return<Paper className={classes.login}><Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper} >
+                <div className={classes.signin}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                </div>
+                {loginButtons()}
+            </div>
+            <Box mt={8}>
+            </Box>
+        </Container>
+        </Paper>
+    }
+
+    const handleLogout = () => {
+        clear()
+        setLogin({})
+    }
+
+
+    return (
+        <Router>
+            <div>
+                <TopBar login={login} handleLogout={handleLogout}/>
+                <main className={classes.content}>
+                    <Route path="/" component={loginComponent}/>
+                    {!isEmpty(login) && <div>
+                        <div className={classes.toolbar}/>
+                        <Route path="/event/new" exact component={CreateEvent}/>
+                        <Route path="/events" exact component={Events}/>
+                        <Route path="/events/:eventId" exact component={Event}/>
+                    </div>}
+                </main>
+            </div>
+        </Router>
+    )
+
 }
 
 export default AppRouter
